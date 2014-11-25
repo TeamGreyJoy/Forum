1955e70fcac5284e4516f095f18cf087eedc409a
Zhan <zhan.ivanovv@gmail.com>
2014-11-25 18:55:37 +0200

    -added html5 login forms loaded from external html file

---
+++ app/scripts/loadLoginForm.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    var contentSection = $('#content');
+    var mainSection = $('main');
+    mainSection.css("background-image", "url(../resources/bgn.jpg)");
+    mainSection.css("background-size", "cover");
+    contentSection.load("../templates/loginForm/index.html");
+});
---
+++ app/scripts/loadQuestions.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var PARSE_APP_ID = "UYWVoDe8RusQmxyZ4HZhkLxYC0sVR0lV6pM8RECE";
+    var PARSE_REST_KEY = "YbUmvG1QEasOSFwt6rvJ8CYEY9T5bESlwxMSWzbY";
+    jQuery.ajax({
+        method: 'GET',
+        headers: {
+            "X-Parse-Application-Id": PARSE_APP_ID,
+            "X-Parse-REST-API-Key": PARSE_REST_KEY
+        },
+        url: "https://api.parse.com/1/classes/Questions"
+    }).success(function(data){
+        for(var i = 0, j = 1; i < data.results.length; i++, j++){
+            var objArray = data.results;
+            var questionObj = objArray[i];
+            var questionText = questionObj.Questions;
+            var element = $('#content :nth-child(' + j + ') h2')[0];
+            var content = document.createTextNode(questionText);
+            element.appendChild(content);
+        }
+//        for(var obj in data.results){
+//            console.log(data.results);
+//            var questionText = obj[1].Questions;
+//            var element = $('#content:nth-child(2) h2');
+//            element.text(questionText);
+//        }
+    });
+});
