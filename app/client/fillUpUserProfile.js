f19c4bc07364577945d84e7600d5a761972fc9a8
Zhan <zhan.ivanovv@gmail.com>
2014-12-06 15:44:31 +0200

    #28 - added "see other user profile" functionality

--- app/client/fillUpUserProfile.js
+++ app/client/fillUpUserProfile.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+function fillProfile(userId){
     var username = $('#username');
     var name = $('#name');
     var memberSince = $('#member');
@@ -8,29 +8,46 @@ $(document).ready(function(){
     var skype = $('#skype');
     var facebook = $('#facebook');
 
-    username.attr('value', cookie.get('username'));
-    name.attr('value', cookie.get('name'));
-    memberSince.attr('value', cookie.get('created'));
-    dateOfBirth.attr('value', cookie.get('dateOfBirth'));
-    email.attr('value', cookie.get('email'));
-    phone.attr('value', cookie.get('phone'));
-    skype.attr('value', cookie.get('skype'));
-    facebook.attr('value', cookie.get('facebook'));
+    if(userId){
+        var url = "https://api.parse.com/1/users/" + userId;
+        Ajax.call(url, "GET", null, function(results){
+            console.log(results);
+            username.attr('value', results.username);
+            name.attr('value', results.name);
+            memberSince.attr('value', new Date(results.createdAt).toLocaleDateString());
+            dateOfBirth.attr('value', results.dateOfBirth);
+            email.attr('value', results.email);
+            phone.attr('value', results.phone);
+            skype.attr('value', results.skype);
+            facebook.attr('value', results.facebook);
+        }, "application/json", false)
 
-    $('#save').on('click', function(){
-        name = name.val();
-        dateOfBirth = dateOfBirth.val();
-        phone = phone.val();
-        skype = skype.val();
-        facebook = facebook.val();
+        $('#save').attr("disabled", "disabled");
+    } else{
+        username.attr('value', cookie.get('username'));
+        name.attr('value', cookie.get('name'));
+        memberSince.attr('value', cookie.get('created'));
+        dateOfBirth.attr('value', cookie.get('dateOfBirth'));
+        email.attr('value', cookie.get('email'));
+        phone.attr('value', cookie.get('phone'));
+        skype.attr('value', cookie.get('skype'));
+        facebook.attr('value', cookie.get('facebook'));
 
-        var data = JSON.stringify({"name": name, "dateOfBirth": dateOfBirth, "phone": phone, "skype": skype, "facebook": facebook});
+        $('#save').on('click', function(){
+            name = name.val();
+            dateOfBirth = dateOfBirth.val();
+            phone = phone.val();
+            skype = skype.val();
+            facebook = facebook.val();
 
-        var url = "https://api.parse.com/1/users/" + cookie.get("userId");
-        Ajax.call(url, "PUT", data, function(){
-            Ajax.call(url, "GET", "", function(results){
-                userData.collect(results);
-            }, "application/json", false);
-        }, "application/json", true);
-    });
-});
+            var data = JSON.stringify({"name": name, "dateOfBirth": dateOfBirth, "phone": phone, "skype": skype, "facebook": facebook});
+
+            var url = "https://api.parse.com/1/users/" + cookie.get("userId");
+            Ajax.call(url, "PUT", data, function(){
+                Ajax.call(url, "GET", null, function(results){
+                    userData.collect(results);
+                }, "application/json", false);
+            }, "application/json", true);
+        });
+    }
+}
